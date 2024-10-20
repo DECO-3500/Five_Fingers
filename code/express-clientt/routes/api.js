e43a5518392f2d27a119cf61a765f536b907a295
JING0823 <zj19990823@gmail.com>
2024-10-20 22:51:45 +1000

    Delete code directory

--- code/express-clientt/routes/api.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getUsers, getUserDataByUsername }  = require('../controllers/userController');
-const router = express.Router();
-
-router.get('/users', getUsers);
-router.get('/getUserData', getUserDataByUsername); // New endpoint for fetching user data by username
-module.exports = router;
